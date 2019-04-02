d798e86c9ee2bf2cc503232009570cd3c861f47f
Olabayo Segun Emmanuel <hollasheg@gmail.com>
2019-04-02 13:09:47 -0400

    I stated Version 1.8 Samdos Electronic Past questions on the 28/03/2019

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/timetable', function(req, res) {
+  res.render('timetable', { title: 'Express' });
+});
+router.get('/pq', function(req, res) {
+  res.render('pq', { title: 'Express' });
+});
+router.get('/solution', function(req, res) {
+  res.render('solution', { title: 'Express' });
+});
+
 module.exports = router;
